df4de812ed73ff324d14350f67f968a64ff44e65
Jina Strickland <jina30@gmail.com>
2020-06-19 01:06:03 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   if position_taken? = false
-    returns = true
+    returns "true"
   else
-    execute something else
+    returns "false"
   end
 end
 
